e99479932f012bc3f69585be8ed2ae4aeee826a5
Khhhgh <103472630+Khhhgh@users.noreply.github.com>
2023-09-16 20:21:54 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,114 @@
+import json
+import os
+
+import requests
+from database import get_db_general_rtb
+from utils import get_restarted
+
+super_sudoers = [5997009476]
+
+
+####################################################################################
+
+# start
+
+wr = get_restarted()
+if wr is None:
+    if os.path.exists('info.json'):
+        fileSize = os.path.getsize("info.json")
+        if fileSize == 0:
+            
+            tokenBot = '6393751475:AAFR2BWat1NiV6zY7WcjYXtFfnX0Eo0gmF8' 
+            
+            idSudo = 5997009476 
+
+            aDict = {"Token": tokenBot, "idSudo": int(idSudo)}
+            jsonString = json.dumps(aDict)
+            jsonFile = open("info.json", "w")
+            jsonFile.write(jsonString)
+            jsonFile.close()
+    else:
+        
+        tokenBot = '6393751475:AAFR2BWat1NiV6zY7WcjYXtFfnX0Eo0gmF8'
+        
+        idSudo = 5997009476
+
+        aDict = {"Token": tokenBot, "idSudo": int(idSudo)}
+        jsonString = json.dumps(aDict)
+        jsonFile = open("info.json", "w")
+        jsonFile.write(jsonString)
+        jsonFile.close()
+
+####################################################################################
+
+# Bot token from Bot Father
+
+# TOKEN = "1619909211:AAFAcQn1FR1aaFloF9hhM5e5vcDTT6MeycQ"
+f = open('info.json', )
+data = json.load(f)
+TOKEN = data['Token']
+
+
+# Your API ID and Hash from https://my.telegram.org/apps
+API_ID = 25996320
+API_HASH = "772cefc3a92ed382b6c24adbd0d3ea26"
+
+# Chat used for logs
+log_chat = 5997009476
+# Sudoers and super sudoers
+sudoers = [data['idSudo']]
+sudoers += super_sudoers
+developer = [5997009476]
+developer += sudoers
+f.close()
+
+
+####################################################################################
+
+def dev():
+    lang = get_db_general_rtb("developer")
+    lang2 = get_db_general_rtb("secdeveloper")
+    if lang is None:
+        print("No Developer")
+    else:
+        for row in lang:
+            t = row[0]
+            developer.append(t)
+    if lang2 is None:
+        print("No Second Devoloper")
+    else:
+        for row in lang2:
+            t = row[0]
+            developer.append(t)
+    print(developer)
+
+
+def get_bot_information():
+    bot_inf = requests.get(
+        "https://api.telegram.org/bot" + TOKEN + "/getme")
+    bot_info = bot_inf.json()
+    result = bot_info["result"]
+    bot_id = result["id"]
+    bot_username = result["username"]
+    return bot_id, bot_username
+
+
+#####################################################################################
+
+
+# Prefixes for commands, e.g: /command and !command
+prefix = ["/", "!"]
+
+# List of disabled plugins
+disabled_plugins = []
+
+# API keys
+TENOR_API_KEY = "2MAL8NKBOO01"
+
+# Bot version, do not touch this
+with open("version.txt") as f:
+    version = f.read().strip()
+
+
+# Run function
+dev()
